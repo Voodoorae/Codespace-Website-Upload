c90e5d20ec83411765bc6d202286f9151b0aec26
Rachael Cousins <143133346+Voodoorae@users.noreply.github.com>
2024-08-13 15:29:48 +0100

    Add files via upload

---
+++ java.js
@@ -0,0 +1,16 @@
+
+
+
+    let sum = 0;
+    for (let i = 0; i < array.length; i++) {
+      sum += array[i];
+    }
+    return sum;
+  
+  
+  const Array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  const result = sumArrayValues(Array);
+  console.log("Sum of the array elements:", result);
+  
+
+
